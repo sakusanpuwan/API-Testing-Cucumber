303f320230cf3c765e9f1614f8ddafbce0c4c08f
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-07-24 16:19:38 +0100

    Added features+step defs for new customer post

--- Tests/features/api-step-definitions.js
+++ Tests/features/api-step-definitions.js
@@ -6,6 +6,8 @@ let statusCode;
 let firstname;
 let secondname;
 let ID;
+let email;
+let deposit;
 
 Given('Execute Test Case {string}', async function (testname){
     console.log("----------------------------------------------------------------");
@@ -13,13 +15,20 @@ Given('Execute Test Case {string}', async function (testname){
 })
 
 
-Given('Customer firstname is {string} and secondname is {string}', async function (name1,name2) {
-    firstname = name1;
-    secondname = name2;
+Given('Customer firstname is {string} and secondname is {string}', async function (inputFirstName,inputSecondName) {
+    firstname = inputFirstName;
+    secondname = inputSecondName;
 })
 
 Given('Customer ID is {string}', async function (string){
     ID = string;
+})
+
+Given('Customer firstname is {string} and secondname is {string} and email is {string} and deposit is {int}', async function(inputFirstName,inputSecondName,inputEmail,inputDeposit) {
+    firstname = inputFirstName;
+    secondname = inputSecondName;
+    email = inputEmail;
+    deposit = inputDeposit;
 })
 
 When('Perform GET operation for endpoint {string} and verify status is {int} and get response', async function (endpoint,status){
@@ -45,9 +54,44 @@ When('Perform GET operation for endpoint {string} and verify status is {int} and
     }
 })
 
+When('Perform POST operation for endpoint {string} and verify status is {int} and get response', async function (endpoint,status){
+    newCustomer = {
+        "firstName": firstname,
+        "secondName": secondname,
+        "email": email,
+        "deposit": deposit
+    }
+    console.log(`Performing POST request for endpoint: ${endpoint}`);
+    let postReponse = await performPOSTRequest(endpoint,newCustomer);
+    
+    console.log(responseData);
+
+    if (status == statusCode) {
+        console.log(`Status ${status} is verified`);
+    } else{
+        console.log(`Incorrect status ${statusCode} is obtained`);
+    }
+
+})
+
 async function performGETRequest(endpoint){
     try {
         const response = await fetch(`${baseURL}${endpoint}`);
+        const data = await response.json();
+        responseData = data;
+        statusCode = response.status
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function performPOSTRequest(endpoint,newCustomer){
+    try {
+        const response = await fetch(`${baseURL}${endpoint}`,{
+            method:"POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(newCustomer)
+    });
         const data = await response.json();
         responseData = data;
         statusCode = response.status
--- Tests/features/bank-api-test.feature
+++ Tests/features/bank-api-test.feature
@@ -22,3 +22,9 @@ Scenario: perform health check - return customer by ID
     Given Execute Test Case "Bank API Health Check 004 : Perform Health Check - Return Customer By ID"
     Given Customer ID is "64bc0aabd31b687f5c6effdd"
     When Perform GET operation for endpoint "/bank-api/customer/{id}" and verify status is 200 and get response
+
+@HealthCheck
+Scenario: perform health check - post new customer
+    Given Execute Test Case "Bank API Health Check 005 : Perform Health Check - Post New Customer"
+    Given Customer firstname is "Bruce" and secondname is "Wayne" and email is "BruceWayne@wayne.com" and deposit is 1000000
+    When Perform POST operation for endpoint "/bank-api/customer" and verify status is 201 and get response
