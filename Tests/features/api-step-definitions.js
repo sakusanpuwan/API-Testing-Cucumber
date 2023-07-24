03941666e02b378752cc97e303d04b7747932567
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-07-24 15:42:35 +0100

    Added features+step defs for name & ID get request

--- Tests/features/api-step-definitions.js
+++ Tests/features/api-step-definitions.js
@@ -3,16 +3,46 @@ const { Given, When } = require("@cucumber/cucumber");
 const baseURL = 'http://localhost:8080';
 let responseData;
 let statusCode;
+let firstname;
+let secondname;
+let ID;
 
 Given('Execute Test Case {string}', async function (testname){
     console.log("----------------------------------------------------------------");
     console.log(`Executing Test Case ${testname}`);
 })
 
+
+Given('Customer firstname is {string} and secondname is {string}', async function (name1,name2) {
+    firstname = name1;
+    secondname = name2;
+})
+
+Given('Customer ID is {string}', async function (string){
+    ID = string;
+})
+
 When('Perform GET operation for endpoint {string} and verify status is {int} and get response', async function (endpoint,status){
+    if (endpoint.includes('name') && firstname.length != 0 && secondname.length != 0) {
+        endpoint += `?firstName=${firstname}&secondName=${secondname}`;
+    } else if (endpoint.includes('name') && firstname.length == 0 && secondname.length != 0){
+        endpoint += `?secondName=${secondname}`;
+    }
+
+    if (endpoint.includes('{id}')) {
+        endpoint = endpoint.replace('{id}',ID);
+    }
+
+    console.log(`Performing GET request for endpoint: ${endpoint}`);
     let getReponse = await performGETRequest(endpoint);
     console.log(responseData);
-    console.log(statusCode);
+
+
+    if (status == statusCode) {
+        console.log(`Status ${status} is verified`);
+    } else{
+        console.log(`Incorrect status ${statusCode} is obtained`);
+    }
 })
 
 async function performGETRequest(endpoint){
@@ -25,3 +55,4 @@ async function performGETRequest(endpoint){
         console.log(error);
     }
 }
+
--- Tests/features/bank-api-test.feature
+++ Tests/features/bank-api-test.feature
@@ -3,4 +3,22 @@ Feature: API test cases for Bank API endpoint health check
 @HealthCheck
 Scenario: perform health check - return all customers
     Given Execute Test Case "Bank API Health Check 001 : Perform Health Check - Return All Customers"
-    When Perform GET operation for endpoint "/bank-api/customer" and verify status is 200 and get response
+    When Perform GET operation for endpoint "/bank-api/customer/all" and verify status is 200 and get response
+
+@HealthCheck
+Scenario: perform health check - return customer by fullname
+    Given Execute Test Case "Bank API Health Check 002 : Perform Health Check - Return Customer By Full Name"
+    Given Customer firstname is "John" and secondname is "Doe"
+    When Perform GET operation for endpoint "/bank-api/customer/name" and verify status is 200 and get response
+
+@HealthCheck
+Scenario: perform health check - return customer by secondname
+    Given Execute Test Case "Bank API Health Check 003 : Perform Health Check - Return Customer By Second Name"
+    Given Customer firstname is "" and secondname is "Doe"
+    When Perform GET operation for endpoint "/bank-api/customer/name" and verify status is 200 and get response
+
+@HealthCheck
+Scenario: perform health check - return customer by ID
+    Given Execute Test Case "Bank API Health Check 004 : Perform Health Check - Return Customer By ID"
+    Given Customer ID is "64bc0aabd31b687f5c6effdd"
+    When Perform GET operation for endpoint "/bank-api/customer/{id}" and verify status is 200 and get response
